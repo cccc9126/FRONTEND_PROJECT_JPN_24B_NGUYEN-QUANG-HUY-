af6d4c7bb76229ac945c70ef1420d4f7e593dcea
cccc9126 <huyhuy9126@gmail.com>
2025-03-30 13:03:51 +0700

    hello

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("signUp").addEventListener("click", function () {
+    window.location = "./page/sign_up.html";
+})
+document.getElementById("signIn").addEventListener("click", function () {
+    window.location = "./page/sign_in.html"; 
+})
